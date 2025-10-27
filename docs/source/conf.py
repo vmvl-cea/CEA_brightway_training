7e4f592b868a50e5e59b4127fc80a1b2073616ab
vm256232 <victor.maneval@cea.fr>
2025-10-27 22:08:45 +0100

    Myst_nb

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "myst-nb"]
+extensions = ["myst_parser", "myst_nb"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
